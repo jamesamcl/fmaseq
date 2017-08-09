46cc6c2befc33a0dba1162949c71bd6f7f309a72
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-08-09 20:41:15 +0100

    faster mirror

--- download_uniprot.sh
+++ download_uniprot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
-wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
 
